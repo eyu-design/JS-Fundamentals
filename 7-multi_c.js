48d4802665c19e5bfca37ae96604a7212933f36e
eyu-design <eyobm0914@gmail.com>
2025-05-27 18:03:48 +0000

    7-multi c.js is added

---
+++ 7-multi_c.js
@@ -0,0 +1,10 @@
+const [, , arg] = process.argv;
+const x = Number(arg);
+
+if (!Number.isInteger(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+}
